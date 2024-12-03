941fb47f210c14ed755435920a9c1689e5414151
raghu22556 <nani.raghu007@gmail.com>
2024-12-03 15:35:50 +0530

    modify

--- jwt_auth_project/settings.py
+++ jwt_auth_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Aspyr12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.53.81.202' 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
